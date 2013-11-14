1a6615f10acc7b442ba354a2b0367f4fe43144b2
MrGelataio <riccardodc@MEGATRON.local>
2013-11-14 18:30:17 +0100

    .

--- 7day_d1/WebFolder/application/loginUser.waComponent/loginUser.js
+++ 7day_d1/WebFolder/application/loginUser.waComponent/loginUser.js
@@ -13,12 +13,33 @@ function constructor (id) {
 	this.load = function (data) {// @lock
 
 	// @region namespaceDeclaration// @startlock
+	var entra = {};	// @button
 	// @endregion// @endlock
 
 	// eventHandlers// @lock
 
+	entra.click = function entra_click (event)// @startlock
+	{// @endlock
+        //Verifico la connessione al DB
+		var db=indexMenu.testdb(1);
+		if(db==0)
+		{
+			alert("DataBase non configurato. Impossibile proseguire");
+			return;
+		}
+        
+        alert(indexMenu.loginD1(1,$$(getHtmlId('d1coduten')).getValue(), $$(getHtmlId('d1genpass')).getValue()));
+	};// @lock
+
 	// @region eventManager// @startlock
+	WAF.addListener(this.id + "_entra", "click", entra.click, "WAF");
 	// @endregion// @endlock
+
+		if(typeof(data.userData) == "undefined")
+		{
+			window.location='/indexMenu.waPage';
+		}
+		       
 
 	};// @lock
 
---
+++ 7day_d1/WebFolder/indexMenu.waPage/scripts/index.js
@@ -0,0 +1,20 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var documentEvent = {};	// @document
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
+	{// @endlock
+
+		$$('main1').loadComponent({path: '/application/loginUser.waComponent', userData: {execute: true}});
+		
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
+// @endregion
+};// @endlock
